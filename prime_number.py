5b0dfd3f3f704ff159f03fce3b68154bdc8524f4
YuYuSlime <slimetomato2500@gmail.com>
2023-08-13 02:07:47 +0900

    確認課題関数の回答

--- prime_number.py
+++ prime_number.py
@@ -1,17 +1,27 @@
-a = input("aの値を入力: ")
-
 # TODO
-a = int(a)
-i = 2
-if a == 1:
-    print("1です")
-elif a < 0:
-    print("負の数です")
-else:
-    while i * i <= a:
-        if a % i == 0:
-            print("合成数です")
-            break
+def prime(number: int):
+    if not isinstance(number, int):
+        raise ValueError("入力がint型ではありません。")
+    if number <= 0:
+        raise ValueError("入力が自然数ではありません。")
+    if number == 1:
+        return False
+    i = 2
+    while i * i <= number:
+        if number % i == 0:
+            return False
         i += 1
     else:
-        print("素数です")
+        return True
+
+
+# examination
+prime(10)
+prime(2)
+prime(61)
+prime(1)
+prime(0)
+prime(3.14)
+prime(-10)
+prime("characters")
+type(prime(10))
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,16 +1,25 @@
-from math import sin, pi
+from math import sin, pi, sqrt, exp
 
-# --example--
-a = 0
-b = 0.5 * pi
 
-n = 100
-h = (b - a) / n
+# TODO
+def func2(x):
+    return 4 / (1 + x**2)
 
-area = 0
-for k in range(1, n + 1):
-    left_end = sin(a + (k - 1) * h)
-    right_end = sin(a + k * h)
-    area += (left_end + right_end) * h / 2
 
-print(area)
+def func3(x):
+    return sqrt(pi) * exp(-(x**2))
+
+
+def trapezoidal_integral(f, a=0, b=1, n=100):
+    h = (b - a) / n
+    area = 0
+    for k in range(n):
+        left_end = f(a + k * h)
+        right_end = f(a + (k + 1) * h)
+        area += (left_end + right_end) * h / 2
+    return area
+
+
+print(trapezoidal_integral(sin, 0, pi / 2, 50))
+print(trapezoidal_integral(func2, 0, 1, 100))
+print(trapezoidal_integral(func3, -100, 100, 1000))
